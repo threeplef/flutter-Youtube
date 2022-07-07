3846e0a8f37b3ebd520b3f10ce840ba7a0a453df
minhaneul <onlynforever@gmail.com>
2022-07-07 14:47:48 +0900

    Youtube Screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_screen/kakao_t/kakao_main_screen.dart';
 import 'package:youtube_screen/youtube_list_screen.dart';
 
 void main() {
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const KaKaoMainScreen(),
+      home: const YoutubeListScreen(),
     );
   }
 }
